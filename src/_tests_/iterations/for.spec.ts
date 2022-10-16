5c5b8c5258dd43a0e321b140c99c29638066d67a
AD <ashokdey100@gmail.com>
2022-10-16 20:03:12 +0530

    fix: skip test

---
+++ src/_tests_/iterations/for.spec.ts
@@ -0,0 +1,10 @@
+import { Parser } from '../../parser/parser';
+
+describe('Testing For Statements', () => {
+  it.skip('Should return a ForStatement', () => {
+    const parser = new Parser();
+    const ast = parser.parse(`;`);
+    const res = JSON.parse(``);
+    expect(ast).toMatchObject(res);
+  });
+});
